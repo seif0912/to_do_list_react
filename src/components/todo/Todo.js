e31fb972a4a69f099ce4a292eaf1cae6d0b5397a
seif0912 <baddredineseif@gmail.com>
2022-12-25 10:24:00 +0100

    ability to get a task done

--- src/components/todo/Todo.js
+++ src/components/todo/Todo.js
@@ -4,16 +4,22 @@ import { FaTrash } from 'react-icons/fa';
 import { AiOutlineCheckCircle, AiFillCheckCircle } from 'react-icons/ai';
 
 const Todo = (props) => {
+    const checkHandler = () => {
+        const temp = [...props.list]
+        temp[props.ind].isDone = !temp[props.ind].isDone
+        props.setList( temp );
+    }
     const trashHandler = () => {
         const temp = [...props.list]
         temp.splice(props.ind, 1)
         props.setList( temp );
     }
     return (
-        <div className="todo">
+        <div className={`todo ${props.isDone ? 'done' : ''}`}>
+        {/* <div className={`todo done`}> */}
             <p>{props.task}</p>
             <div className="controls">
-                <div className="check">
+                <div className="check" onClick={checkHandler}>
                     {props.isDone ? <AiFillCheckCircle/> :<AiOutlineCheckCircle/>}
                 </div>
                 <div className="trash" onClick={trashHandler}>
