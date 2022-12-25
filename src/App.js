108823c1c4e3c11922c246f963cb2ca547bfbf67
seif0912 <baddredineseif@gmail.com>
2022-12-25 12:17:10 +0100

    local storage functionalities

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import {Container, Todos, Input} from './components/Index'
 import './App.css';
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 
 
 function App() {
   let [list, setList] = useState([])
+  function getTasks() {
+      let tasksL;
+      if (localStorage.getItem('tasksL') === null) {
+          tasksL = [];
+      } else {
+          tasksL = JSON.parse(localStorage.getItem('tasksL'));
+      }
+      tasksL.forEach(function(task) {
+        setList(prev => [...prev, task])
+      });
+  }
+  useEffect(getTasks,[])
 
   return (
     <div className="App">
